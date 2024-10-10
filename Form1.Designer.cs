4aded8fb2ff7da6c70ef749f1f69766831ffacd7
vlaadoos <90085802+vlaadoos@users.noreply.github.com>
2024-10-10 11:36:15 +0500

    MATRIXX
    Дана квадратная вещественная матрица размерности n. Сравнить сумму элементов матрицы на главной и побочной диагоналях.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(750, 300);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(12, 329);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(100, 20);
+            this.txtSize.TabIndex = 1;
+            this.txtSize.TextChanged += new System.EventHandler(this.txtSize_TextChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(687, 415);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Вычислить";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(327, 363);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "label1";
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int n;
+            if (!int.TryParse(txtSize.Text, out n) || n <= 0)
+            {
+                MessageBox.Show("Введите корректное значение размерности матрицы (n > 0).");
+                return;
+            }
+
+            double[,] matrix = new double[n, n];
+
+            // Считывание данных из DataGridView
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value != null)
+                    {
+                        if (!double.TryParse(dataGridView1.Rows[i].Cells[j].Value.ToString(), out matrix[i, j]))
+                        {
+                            MessageBox.Show("Введите корректные вещественные значения в матрице.");
+                            return;
+                        }
+                    }
+                }
+            }
+
+            // Вычисление сумм главной и побочной диагоналей
+            double sumMainDiagonal = 0;
+            double sumSecondaryDiagonal = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sumMainDiagonal += matrix[i, i];           // Элементы главной диагонали
+                sumSecondaryDiagonal += matrix[i, n - i - 1]; // Элементы побочной диагонали
+            }
+
+            // Вывод результатов
+            lblResult.Text = $"Сумма главной диагонали: {sumMainDiagonal}\n" +
+                             $"Сумма побочной диагонали: {sumSecondaryDiagonal}\n";
+
+            if (sumMainDiagonal > sumSecondaryDiagonal)
+                lblResult.Text += "Сумма главной диагонали больше.";
+            else if (sumMainDiagonal < sumSecondaryDiagonal)
+                lblResult.Text += "Сумма побочной диагонали больше.";
+            else
+                lblResult.Text += "Суммы равны.";
+        }
+
+        private void txtSize_TextChanged(object sender, EventArgs e)
+        {
+            int n;
+            if (int.TryParse(txtSize.Text, out n) && n > 0)
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                // Создание столбцов и строк в DataGridView
+                for (int i = 0; i < n; i++)
+                {
+                    dataGridView1.Columns.Add($"Column{i}", $"Column {i + 1}");
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    dataGridView1.Rows.Add(new object[n]);
+                }
+            }
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
+
